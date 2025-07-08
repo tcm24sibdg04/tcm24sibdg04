17585c771bcdee92a83f234dc1b4b4c9a975c5bb
irisCorreia <a043481@umaia.pt>
2025-07-09 00:12:33 +0100

    Delete scr directory

--- scr/models/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './produto.model';
-export * from './receita.model';
-export * from './utilizador.model';
-export * from './pedido.model';
-export * from './desperdicio.model';
-export * from './turno.model';
-export * from './ingrediente-receita.model';
